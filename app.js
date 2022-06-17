dbf80c950edff9b4d9efd37c20bb35b3163c0f49
dev1-ish <alma.miguez30@gmail.com>
2022-06-17 16:24:25 -0300

    [updated] funtionality js done (simple loop)

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // VID: https://www.youtube.com/watch?v=RVrHC__Tkx0
+function searchPlant() {
+    // DOM
+    const input = document.getElementById('filter').value.toUpperCase();
+    // console.log(input)
+
+    const cardContainer = document.getElementById('card-lists');
+
+    const cards = cardContainer.getElementsByClassName('card');
+
+    for (let i = 0; i < cards.length; i++){
+        let title = cards[i].querySelector(".card-body h5.card-title");
+
+        if (title.innerText.toUpperCase().indexOf(input) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
